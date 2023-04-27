8b9df2cf87c25ba057c5c9e034f93aab18c2ec0a
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2023-04-27 14:54:12 +0530

    youtube & google search and open result in same tab

--- script.js
+++ script.js
@@ -55,3 +55,59 @@ setInterval(set_time, 1000)
 set_quote()
 setInterval(refresh_quote, 600000)
 
+// function youtube_search(){
+//     var yt_search_field = document.getElementById("youtube-search-input").value
+//     console.log(yt_search_field)
+// }
+
+document.getElementById("youtube-search-btn").addEventListener('click', () => {
+    console.log("youtube search button clicked");
+    got_for_youtube_search();
+})
+
+document.getElementById("google-search-btn").addEventListener('click', () => {
+    console.log("google  button clicked");
+    got_for_google_search();
+})
+
+document.getElementById("youtube-search-input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        console.log("youtube enter key pressed")
+        got_for_youtube_search();
+    }
+});
+
+document.getElementById("google-search-input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        console.log("google enter key pressed")
+        got_for_google_search();
+    }
+});
+
+const youtube_search_link = "https://www.youtube.com/results?search_query=";
+const google_search_link = "https://www.google.com/search?q=";
+
+function got_for_google_search(){
+    got_for_search("google-search-input",google_search_link)
+}
+
+function got_for_youtube_search(){
+    got_for_search("youtube-search-input",youtube_search_link)
+}
+
+function got_for_search(input_element_id, main_link) {
+
+    var search_quey = document.getElementById(input_element_id).value;
+    console.log(search_quey)
+
+    if (search_quey == "") {
+        console.log("empty search query")
+    } else {
+        search_quey = search_quey.replaceAll(" ", "+");
+        window.open(main_link + search_quey, "_parent");
+        document.getElementById(input_element_id).value = "";
+    }
+
+}
